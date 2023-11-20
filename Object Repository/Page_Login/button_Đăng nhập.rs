77a6183a2fb1f06a0f1a3860eb7937ca4e321f0f
Hoàng Minh <minhhv2@msb.com.vn>
2023-11-20 10:45:42 +0700

    minhhv2 add project BOSBO

---
+++ Object Repository/Page_Login/button_Đăng nhập.rs
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_đăng nhập</name>
+   <tag></tag>
+   <elementGuidId>75105f47-49e3-473a-b3e7-63b4eb3d6dc9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn.btn-primary.px-4.submit</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@value='Login']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>f63271b8-7050-4614-8bd9-f828b8142032</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>value</name>
+      <type>Main</type>
+      <value>Login</value>
+      <webElementGuid>87b4e0ee-11d3-4d65-8ec2-dd28bdbfa645</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+      <webElementGuid>76fcc381-0b22-44e5-a42a-f8ece08a8d7f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary px-4 submit</value>
+      <webElementGuid>19b67359-17f7-4c32-88ad-b84dc2eed58d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                Đăng nhập
+              </value>
+      <webElementGuid>33abd0b3-8953-4ecf-b762-f20d28545213</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;login-body&quot;]/div[@class=&quot;row login-main&quot;]/div[@class=&quot;col-md-5&quot;]/form[1]/div[@class=&quot;p-text-center&quot;]/button[@class=&quot;btn btn-primary px-4 submit&quot;]</value>
+      <webElementGuid>85bc8624-58a6-439c-91b9-6758f2383f70</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@value='Login']</value>
+      <webElementGuid>0bc207f7-158e-4765-882a-ba589f5c41c0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+      <webElementGuid>19bbbcb9-bf3e-40aa-9f6d-fe8395117fae</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'submit' and (text() = '
+                Đăng nhập
+              ' or . = '
+                Đăng nhập
+              ')]</value>
+      <webElementGuid>82acd8ee-0ce7-40f8-841b-3e839601b9b4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
